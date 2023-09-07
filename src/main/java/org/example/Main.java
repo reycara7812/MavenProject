aae44891f6b7549f4a7b5170a42e9888a4cb862d
Henry Santos <reycara7812@gmail.com>
2023-09-07 13:14:35 -0400

    This is app dev hw

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My name is Henry");
+    }
+}
